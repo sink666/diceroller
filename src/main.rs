d454562afd62c4d8ba5d008ad958d6304ef7e1e1
sink666 <er.basilisk@gmail.com>
2025-06-17 12:54:57 +0200

    skip errors in a nice way

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::fmt;
-
 use rand::prelude::*;
+use std::{
+    error, fmt,
+    num::{NonZeroU8, ParseIntError},
+};
 
 //
 // roll sets of dice with dice notation and output rolls to a text file
@@ -24,12 +26,75 @@ const DICE_STRUCT: DiceStruct = DiceStruct {
     ],
 };
 
+#[derive(Debug)]
+enum DiceErrorKind {
+    InputSyntax,
+    UnrecognizedDieType,
+    ParseNumber(ParseIntError),
+}
+
+impl fmt::Display for DiceErrorKind {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            DiceErrorKind::InputSyntax => write!(f, "faulty dice syntax"),
+            DiceErrorKind::UnrecognizedDieType => write!(f, "unrecognized dice type"),
+            DiceErrorKind::ParseNumber(e) => write!(f, "parse number: {}", e),
+        }
+    }
+}
+
+impl error::Error for DiceErrorKind {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match *self {
+            DiceErrorKind::InputSyntax => None,
+            DiceErrorKind::UnrecognizedDieType => None,
+            DiceErrorKind::ParseNumber(ref e) => Some(e),
+        }
+    }
+}
+
+impl From<ParseIntError> for DiceErrorKind {
+    fn from(err: ParseIntError) -> DiceErrorKind {
+        DiceErrorKind::ParseNumber(err)
+    }
+}
+
+#[derive(Debug)]
+struct DiceParseError {
+    e_type: DiceErrorKind,
+    context: String,
+}
+
+impl fmt::Display for DiceParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}, at: {}", self.e_type, self.context)
+    }
+}
+
 enum DieType {
     D4,
     D6,
     D8,
     D10,
     D20,
+}
+
+impl TryFrom<u8> for DieType {
+    type Error = DiceParseError;
+
+    fn try_from(item: u8) -> Result<Self, Self::Error> {
+        match item {
+            4 => Ok(DieType::D4),
+            6 => Ok(DieType::D6),
+            8 => Ok(DieType::D8),
+            10 => Ok(DieType::D10),
+            20 => Ok(DieType::D20),
+            _ => Err(DiceParseError {
+                e_type: DiceErrorKind::UnrecognizedDieType,
+                context: item.to_string(),
+            }),
+        }
+    }
 }
 
 impl fmt::Display for DieType {
@@ -64,38 +129,58 @@ impl DiceRoll {
     }
 }
 
-fn roll_parser(dstr: &str) -> DiceRoll {
+fn roll_parser(dstr: &str) -> Result<DiceRoll, DiceParseError> {
     // number of dice, dice type
-    let roll_tup: (u8, u8) = {
-        let tmp1: Vec<u8> = dstr.split('d').map(|t| t.parse::<u8>().unwrap()).collect();
-        (tmp1[0], tmp1[1])
+    let roll_tup: (NonZeroU8, u8) = {
+        let num = match dstr.split_once('d') {
+            Some((a, b)) => Ok((a, b)),
+            None => Err(DiceParseError {
+                e_type: DiceErrorKind::InputSyntax,
+                context: dstr.into(),
+            }),
+        }?;
+
+        let lval = match num.0.parse::<NonZeroU8>() {
+            Ok(r) => r,
+            Err(e) => {
+                return Err(DiceParseError {
+                    e_type: DiceErrorKind::ParseNumber(e),
+                    context: dstr.into(),
+                });
+            }
+        };
+        let rval = match num.1.parse::<u8>() {
+            Ok(r) => r,
+            Err(e) => {
+                return Err(DiceParseError {
+                    e_type: DiceErrorKind::ParseNumber(e),
+                    context: dstr.into(),
+                });
+            }
+        };
+
+        (lval, rval)
     };
 
-    DiceRoll {
-        dietype: match roll_tup.1 {
-            4 => DieType::D4,
-            6 => DieType::D6,
-            8 => DieType::D8,
-            10 => DieType::D10,
-            20 => DieType::D20,
-            _ => panic!("unknown die type!!"),
-        },
-        num_rolls: match roll_tup.0 {
-            0 => panic!("cannot roll zero dice!!"),
-            _ => roll_tup.0,
-        },
+    Ok(DiceRoll {
+        dietype: roll_tup.1.try_into()?,
+        num_rolls: roll_tup.0.into(),
         results: Vec::new(),
-    }
+    })
 }
 
 fn main() {
-    let instr = String::from("20d6 20d4 20d20");
+    let instr = String::from("1x6 0d4 9d6 20d20");
+    // let instr = String::from("1d6 1d4 3d6 2d20");
     let mut rng = rand::rng();
     let roll_list: Vec<&str> = instr.split_whitespace().collect();
     let mut dice_parsed: Vec<DiceRoll> = Vec::new();
 
     for roll_str in roll_list {
-        dice_parsed.push(roll_parser(roll_str));
+        match roll_parser(roll_str) {
+            Ok(x) => dice_parsed.push(x),
+            Err(e) => println!("error: {e}. continuing..."),
+        }
     }
 
     for roll in &mut dice_parsed {
